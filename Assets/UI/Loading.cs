78562265fc5957aa520688e7bd7953394ba8f583
ArTeGaS-2 <probablytorvik@gmail.com>
2024-11-14 19:58:03 +0200

    Loading screen logic

---
+++ Assets/UI/Loading.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Loading : MonoBehaviour
+{
+    public GameObject loadingScreen; // Контейнер для усього екрану завантаження
+
+    // Посилання на зображення крапок
+    public GameObject dot_1;
+    public GameObject dot_2;
+    public GameObject dot_3;
+    private void Start()
+    {
+        StartCoroutine(AnimateDots());
+    }
+    IEnumerator AnimateDots()
+    // Метод для анімування
+    {
+        while (true)
+        {
+            // Збільшення крапки
+            dot_1.transform.localScale =
+                new Vector3(2f,2f,2f);
+            // Затримка в 1 секунду
+            yield return new WaitForSecondsRealtime(1f);
+            // Зменшення крапки
+            dot_1.transform.localScale =
+                new Vector3(1f, 1f, 1f);
+
+            dot_2.transform.localScale =
+                new Vector3(2f, 2f, 2f);
+            yield return new WaitForSecondsRealtime(1f);
+            dot_2.transform.localScale =
+                new Vector3(1f, 1f, 1f);
+
+            dot_3.transform.localScale =
+                new Vector3(2f, 2f, 2f);
+            yield return new WaitForSecondsRealtime(1f);
+            dot_3.transform.localScale =
+                new Vector3(1f, 1f, 1f);
+        }
+    }
+}
