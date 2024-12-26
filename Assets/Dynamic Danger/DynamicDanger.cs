b9ff44ec2c84bb637a3e7bad6b69afd3d62db3bf
ArTeGaS-2 <probablytorvik@gmail.com>
2024-12-26 19:59:38 +0200

    Доповнення скрита руху динамічної загрози

--- Assets/Dynamic Danger/DynamicDanger.cs
+++ Assets/Dynamic Danger/DynamicDanger.cs
@@ -6,19 +6,40 @@ public class DynamicDanger : MonoBehaviour
 {
     public float needToGo = 3f; // Час/затримка до події
     public float enemySpeed = 5f; // Швидкість ворога/об'єкта
+
+    // Змінні під кут обертання
+    public float currentRotationX = 0f; // X
+    public float currentRotationY = 0f; // Y
+    public float currentRotationZ = 0f; // Z
+
+    // Прискорення по осям
+    public float currentSpeedX = 0f; // X
+    public float currentSpeedY = 0f; // Y
+    public float currentSpeedZ = 0f; // Z
     private void Start()
     {
-        StartCoroutine(rotateEnemy());
+        StartCoroutine(RotateEnemy());
     }
-    private IEnumerator rotateEnemy()
+    private void Update()
     {
-        // Затримка перед початком роботи методу у needToGo(3) секунди
-        yield return new WaitForSeconds(needToGo);
-        // Обертання об'єкту
-        transform.rotation = Quaternion.Euler(
-            transform.rotation.x, // x
-            90,                   // y
-            90);                  // z
+        // Зміна позиції
+        transform.position = new Vector3(
+            transform.position.x + currentSpeedX,
+            transform.position.y + currentSpeedY,
+            transform.position.z + currentSpeedZ);
+    }
+    private IEnumerator RotateEnemy()
+    {
+        while (true)
+        {
+            // Затримка перед початком роботи методу у needToGo(3) секунди
+            yield return new WaitForSeconds(needToGo);
+            // Обертання об'єкту
+            transform.rotation = Quaternion.Euler(
+                currentRotationX, // x
+                currentRotationY, // y
+                currentRotationZ);// z
+        }
     }
     private void OnCollisionEnter(Collision collision)
     {
