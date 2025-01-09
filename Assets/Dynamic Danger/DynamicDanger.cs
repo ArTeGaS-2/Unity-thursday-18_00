6860aa98057c969ec74fdde3b70b7a2318ec0703
ArTeGaS-2 <probablytorvik@gmail.com>
2025-01-09 19:57:43 +0200

    Переробили об'єкт і скрипт dynamic danger

--- Assets/Dynamic Danger/DynamicDanger.cs
+++ Assets/Dynamic Danger/DynamicDanger.cs
@@ -5,47 +5,22 @@ using UnityEngine;
 public class DynamicDanger : MonoBehaviour
 {
     public float needToGo = 3f; // Час/затримка до події
-    public float enemySpeed = 5f; // Швидкість ворога/об'єкта
+    public float objectSpeed = 5f; // Швидкість ворога/об'єкта
 
-    // Змінні під кут обертання
-    public float currentRotationX = 0f; // X
-    public float currentRotationY = 0f; // Y
-    public float currentRotationZ = 0f; // Z
-
-    // Прискорення по осям
-    public float currentSpeedX = 0f; // X
-    public float currentSpeedY = 0f; // Y
-    public float currentSpeedZ = 0f; // Z
+    private Rigidbody rb;
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(other.gameObject);
+    }
     private void Start()
     {
-        StartCoroutine(RotateEnemy());
+        rb = GetComponent<Rigidbody>();
     }
     private void Update()
     {
-        // Зміна позиції
-        transform.position = new Vector3(
-            transform.position.x + currentSpeedX,
-            transform.position.y + currentSpeedY,
-            transform.position.z + currentSpeedZ);
-    }
-    private IEnumerator RotateEnemy()
-    {
-        while (true)
-        {
-            // Затримка перед початком роботи методу у needToGo(3) секунди
-            yield return new WaitForSeconds(needToGo);
-            // Обертання об'єкту
-            transform.rotation = Quaternion.Euler(
-                currentRotationX, // x
-                currentRotationY, // y
-                currentRotationZ);// z
-        }
-    }
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.CompareTag("Player"))
-        {
-            Destroy(collision.gameObject);
-        }
+        rb.velocity = Vector3.forward *
+                      objectSpeed *
+                      Time.deltaTime;
+
     }
 }
