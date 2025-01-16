9d61c0a3619447bd0f125947beca36cc914000fd
ArTeGaS-2 <probablytorvik@gmail.com>
2025-01-16 20:05:00 +0200

    Оберання динамічної загрози

--- Assets/Dynamic Danger/DynamicDanger.cs
+++ Assets/Dynamic Danger/DynamicDanger.cs
@@ -8,6 +8,9 @@ public class DynamicDanger : MonoBehaviour
     public float objectSpeed = 5f; // Швидкість ворога/об'єкта
 
     private Rigidbody rb;
+
+    private float currentAngleMod = 0f; // Поточний модифікатор напрямку
+    public float anglePerIteration = 90f; // Зміна кута за ітерацію
     private void OnTriggerEnter(Collider other)
     {
         Destroy(other.gameObject);
@@ -15,12 +18,29 @@ public class DynamicDanger : MonoBehaviour
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
+        StartCoroutine(RotateDynamicDanger());
     }
     private void Update()
     {
-        rb.velocity = Vector3.forward *
+        rb.velocity = Vector3.left *
                       objectSpeed *
                       Time.deltaTime;
 
+    }
+    private IEnumerator RotateDynamicDanger()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(needToGo); // затримка у секундах
+                                                       // Зміна поточного кута
+
+            Vector3 currenEulerAngles = transform.rotation.eulerAngles;
+
+            currenEulerAngles.z += currentAngleMod;
+
+            transform.rotation = Quaternion.Euler(currenEulerAngles);
+
+            currentAngleMod += anglePerIteration; // Зміна значення модифікатора
+        }
     }
 }
