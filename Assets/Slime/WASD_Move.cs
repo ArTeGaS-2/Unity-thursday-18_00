d6d93acd835e07a2e0cf81eb9858520e5e4da9c0
ArTeGaS-2 <probablytorvik@gmail.com>
2025-03-20 19:55:38 +0200

    Скрипт руху та анімацій

--- Assets/Slime/WASD_Move.cs
+++ Assets/Slime/WASD_Move.cs
@@ -19,4 +19,53 @@ public class WASD_Move : MonoBehaviour
         Instance = this;
         rb = GetComponent<Rigidbody>();
     }
+    private void Update()
+    {
+        // Отримуємо значення вводу з клавіатури
+        Vector3 moveInput = new Vector3(
+            Input.GetAxis("Horizontal"), 0,
+            Input.GetAxis("Vertical"));
+        // Пряме присвоєння швидкості (замість AddForce)
+        if (moveInput.magnitude > 0.1f)
+        {
+            Vector3 dessiredVelocity = moveInput.normalized *
+                maxSpeed;
+            rb.velocity = new Vector3(dessiredVelocity.x,
+                rb.velocity.y, dessiredVelocity.z);
+        }
+        else
+        {
+            rb.velocity = new Vector3 (0, rb.velocity.y, 0);
+        }
+        // Обчислюємо локальний вектор руху
+        // відносно поточного повороту моделі
+        Vector3 localMovement = transform.InverseTransformDirection(
+            moveInput);
+        // Передаємо локальні координати в аніматор
+        animator.SetFloat("VelocityX", localMovement.x);
+        animator.SetFloat("VelocityZ", localMovement.z);
+
+        // Обертання моделі у напрямку миші
+        Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+        Plane plane = new Plane(Vector3.up, transform.position);
+        if (plane.Raycast(ray, out float rayDistance))
+        {
+            Vector3 worldMousePosition = ray.GetPoint(rayDistance);
+            Vector3 directionToMouse = worldMousePosition
+                - transform.position;
+            directionToMouse.y = 0;
+            if (directionToMouse.sqrMagnitude > 0.001f)
+            {
+                Quaternion targetRotation = Quaternion.LookRotation(
+                    directionToMouse);
+                transform.rotation = Quaternion.Lerp(transform.rotation,
+                    targetRotation, Time.deltaTime * rotationSpeed);
+            }
+        }
+        // Оновлення позиції камери
+        mainCamera.transform.position = new Vector3(
+            transform.position.x,
+            cameraDistance,
+            transform.position.z - cameraDistance / 7);
+    }
 }
